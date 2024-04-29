3db0937b4b17ef35dad318066d63c9921851877d
Lars Duelfer <larsduelfer@gmx.de>
2024-04-29 09:53:49 +0200

    Add vuetify and basic layout

--- frontend-new/src/main.ts
+++ frontend-new/src/main.ts
@@ -1,7 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+// OIDC Library
 import './oidc'
 
-createApp(App).use(router).mount('#app')
+// Router
+import router from './router'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+const app = createApp(App).use(router)
+
+registerPlugins(app)
+
+app.mount('#app')
---
+++ frontend-new/src/pages/SignOutPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useAuth } from 'vue3-oidc'
+
+const { signoutRedirect } = useAuth()
+onMounted(() => signoutRedirect())
+</script>
