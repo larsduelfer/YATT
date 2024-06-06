7cd9e281295cd7094d0141f93207b48d6d848aa7
Lars Duelfer <larsduelfer@gmx.de>
2024-06-06 11:12:33 +0200

    Fix dependency in backend

--- backend/clients/rsocket/build.gradle.kts
+++ backend/clients/rsocket/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("yatt.kotlin-conventions")
   id("org.springframework.boot")
+  id("com.google.osdetector") version "1.7.0"
   kotlin("plugin.spring")
   kotlin("jvm")
 }
@@ -8,6 +9,10 @@ plugins {
 group = "${group}.clients"
 
 dependencies {
+  if (osdetector.arch.equals("aarch_64")) {
+    implementation("io.netty:netty-all")
+  }
+
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.springframework.boot:spring-boot-starter-rsocket")
   implementation("org.springframework.security:spring-security-rsocket")
