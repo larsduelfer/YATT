e8c19a7db2530058950492466a5206297c4d328c
Lars Duelfer <larsduelfer@gmx.de>
2024-06-06 16:18:28 +0200

    Upgrade spring shell dependencies of grpc client

--- backend/clients/grpc/build.gradle.kts
+++ backend/clients/grpc/build.gradle.kts
@@ -8,12 +8,14 @@ plugins {
 group = "${group}.clients"
 
 dependencies {
+  implementation(platform("org.springframework.shell:spring-shell-dependencies:3.3.0"))
+
   implementation(project(":apis:grpc-lib"))
 
   implementation("com.google.protobuf:protobuf-java-util:3.19.2")
   implementation("net.devh:grpc-client-spring-boot-starter:2.13.1.RELEASE")
   implementation("org.springframework.boot:spring-boot-starter")
   implementation("org.springframework.boot:spring-boot-starter")
-  implementation("org.springframework.shell:spring-shell-starter:2.1.0-M3")
+  implementation("org.springframework.shell:spring-shell-starter")
   implementation("org.springframework:spring-web")
 }
