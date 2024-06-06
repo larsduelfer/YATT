7781445e74b492019b4fad87fee22ed91bca338d
Lars Duelfer <larsduelfer@gmx.de>
2024-06-06 16:16:07 +0200

    Upgrade spring shell dependencies of rsocket client

--- backend/clients/rsocket/build.gradle.kts
+++ backend/clients/rsocket/build.gradle.kts
@@ -9,6 +9,8 @@ plugins {
 group = "${group}.clients"
 
 dependencies {
+  implementation(platform("org.springframework.shell:spring-shell-dependencies:3.3.0"))
+
   if (osdetector.arch.equals("aarch_64")) {
     implementation("io.netty:netty-all")
   }
@@ -16,5 +18,5 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.springframework.boot:spring-boot-starter-rsocket")
   implementation("org.springframework.security:spring-security-rsocket")
-  implementation("org.springframework.shell:spring-shell-starter:2.1.0-M3")
+  implementation("org.springframework.shell:spring-shell-starter")
 }
