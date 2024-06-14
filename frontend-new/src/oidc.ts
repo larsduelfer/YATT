116cb3cec0799f8d6013ea84f25f1a12446e54d1
Lars Duelfer <larsduelfer@gmx.de>
2024-06-14 08:26:45 +0200

    Add page for silent renew of auth token

--- frontend-new/src/oidc.ts
+++ frontend-new/src/oidc.ts
@@ -9,7 +9,7 @@ const oidcSettings: VueOidcSettings = {
   authority: 'http://localhost:8999/realms/eventsourcing-with-axon/',
   scope: 'openid',
   client_id: 'my-backend',
-  //client_secret: "your client secret",
+  // client_secret: "your client secret",
   redirect_uri: origin + '/oidc-callback',
   post_logout_redirect_uri: origin + '/sign-in',
   response_type: 'code',
@@ -28,13 +28,13 @@ const oidcSettings: VueOidcSettings = {
 
 function runAuth() {
   createOidc({
-    oidcSettings: oidcSettings, //your oidc settings
-    auth: false, //if auth is true,will auto authenticate
+    oidcSettings: oidcSettings, // your oidc settings
+    auth: false, // if auth is true, will auto authenticate
     refreshToken: {
       enable: true,
       time: 10 * 1000
     },
-    //your oidc events
+    // your oidc events
     events: {
       addUserLoaded: (user) => {
         console.log(user)
