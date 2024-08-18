8ada7b075a2079591dc96d6f7eabf2698e948b8b
Lars Duelfer <larsduelfer@gmx.de>
2024-08-18 08:08:20 +0200

    Remove old frontend

--- frontend/src/keycloak.ts
+++
@@ -1,9 +0,0 @@
-import Keycloak from "keycloak-js";
-
-const keycloak = Keycloak({
-    url: 'http://localhost:8999',
-    realm: 'eventsourcing-with-axon',
-    clientId: 'my-backend'
-});
-
-export default keycloak;
